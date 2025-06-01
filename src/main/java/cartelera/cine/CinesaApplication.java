ae651bf75452d96e58de33c2ac2281b8a5fe8e86
ojosdegato <elcomandodelgeek@gmail.com>
2025-06-01 18:08:01 +0200

    Initial commit

---
+++ src/main/java/cartelera/cine/CinesaApplication.java
@@ -0,0 +1,13 @@
+package cartelera.cine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinesaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinesaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cartelera/cine/CinesaApplicationTests.java
@@ -0,0 +1,13 @@
+package cartelera.cine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinesaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
