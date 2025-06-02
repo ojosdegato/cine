d49f7d21ec3d4973ce15951f6d86d7b7bf15c047
Javier Cachón Garrido <javier.cachon@izetic.com>
2025-06-02 12:39:36 +0200

    Add files via upload

---
+++ src/test/java/cartelera/cine/cinesa/CinesaApplicationTests.java
@@ -0,0 +1,13 @@
+package cartelera.cine.cinesa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinesaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
