2a29fa88eae1d5d37baa93dd8a2a5fb3bc5f230f
Javier Cachón Garrido <javier.cachon@izetic.com>
2025-06-02 21:54:31 +0200

    intentando resolver el error:
    Error starting ApplicationContext. To display the condition evaluation report re-run your application with 'debug' enabled.
    2025-06-02T21:51:06.449+02:00 ERROR 13698 --- [cine] [           main] o.s.boot.SpringApplication               : Application run failed
    
    org.springframework.context.ApplicationContextException: Failed to start bean 'webServerStartStop'
    
    Process finished with exit code 1

---
+++ src/main/java/cine/cartelera/cine/CineApplication.java
@@ -0,0 +1,14 @@
+package cine.cartelera.cine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CineApplication.class, args);
+	}
+
+}
