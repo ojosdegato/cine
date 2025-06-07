dee06212a622004ee0dae844acde609fee573e10
D-JMO <davidjimenezotero@gmail.com>
2025-06-07 11:54:35 +0200

    Crear entidad Ticket

--- src/main/java/cine/cartelera/cine/entities/Ticket.java
+++ src/main/java/cine/cartelera/cine/entities/Ticket.java
@@ -1,11 +1,16 @@
 package cine.cartelera.cine.entities;
 
 import jakarta.persistence.*; // o javax.persistence.* si usas Jakarta EE 8 o Java EE
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Entity // Indica que esta clase es una tabla
 @Table(name = "TICKET") // Nombre de la tabla en H2
 public class Ticket {
 
+    // Getters y Setters
     @Id // Indica que este atributo es la clave primaria
     @GeneratedValue(strategy = GenerationType.IDENTITY) // Generación automática de ID
 
@@ -26,39 +31,6 @@ public class Ticket {
     public Ticket(int aforoDeLaSala, int asientosOcupados, Double precioDeLaEntrada) {
         this.aforoDeLaSala = aforoDeLaSala;
         AsientosOcupados = asientosOcupados;
-        PrecioDeLaEntrada = precioDeLaEntrada;
-    }
-
-    // Getters y Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public int getAforoDeLaSala() {
-        return aforoDeLaSala;
-    }
-
-    public void setAforoDeLaSala(int aforoDeLaSala) {
-        this.aforoDeLaSala = aforoDeLaSala;
-    }
-
-    public int getAsientosOcupados() {
-        return AsientosOcupados;
-    }
-
-    public void setAsientosOcupados(int asientosOcupados) {
-        AsientosOcupados = asientosOcupados;
-    }
-
-    public Double getPrecioDeLaEntrada() {
-        return PrecioDeLaEntrada;
-    }
-
-    public void setPrecioDeLaEntrada(Double precioDeLaEntrada) {
         PrecioDeLaEntrada = precioDeLaEntrada;
     }
 
