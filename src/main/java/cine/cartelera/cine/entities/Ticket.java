209ae1b69c3dd643e12970c7538f48ff4ab96910
D-JMO <davidjimenezotero@gmail.com>
2025-06-08 19:33:21 +0200

    Relacionar entidad ticket con la entidad proyección

--- src/main/java/cine/cartelera/cine/entities/Ticket.java
+++ src/main/java/cine/cartelera/cine/entities/Ticket.java
@@ -2,38 +2,79 @@ package cine.cartelera.cine.entities;
 
 import jakarta.persistence.*; // o javax.persistence.* si usas Jakarta EE 8 o Java EE
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Setter
-@Getter
 @Entity // Indica que esta clase es una tabla
 @Table(name = "TICKET") // Nombre de la tabla en H2
 
 public class Ticket {
 
-    // Getters y Setters
-    @Id // Indica que este atributo es la clave primaria
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // Generación automática de ID
-
-    // Atributo para la clave primaria
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // Atributos de la entidad Ticket
-    @Column(nullable = false)
+    // Capacidad total de la sala para esta proyección
     private int aforoDeLaSala;
-    private int AsientosOcupados;
-    private Double PrecioDeLaEntrada;
+
+    // Número de asientos ocupados con este ticket
+    private int asientosOcupados;
+
+    // Precio de la entrada
+    private double PrecioDeLaEntrada;
+
+    // Relación con la proyección a la que pertenece este ticket
+    @ManyToOne
+    @JoinColumn(name = "proyeccion_id")
+    private Proyeccion proyeccion;
 
     // Constructor vacío
-    public Ticket() {
-    }
+    public Ticket() {}
 
     // Constructor con parámetros
-    public Ticket(int aforoDeLaSala, int asientosOcupados, Double precioDeLaEntrada) {
+    public Ticket(int aforoDeLaSala, int asientosOcupados, double PrecioDeLaEntrada, Proyeccion proyeccion) {
         this.aforoDeLaSala = aforoDeLaSala;
-        AsientosOcupados = asientosOcupados;
+        this.asientosOcupados = asientosOcupados;
+        this.PrecioDeLaEntrada = PrecioDeLaEntrada;
+        this.proyeccion = proyeccion;
+    }
+
+    // Getters y Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getAforoDeLaSala() {
+        return aforoDeLaSala;
+    }
+
+    public void setAforoDeLaSala(int aforoDeLaSala) {
+        this.aforoDeLaSala = aforoDeLaSala;
+    }
+
+    public int getAsientosOcupados() {
+        return asientosOcupados;
+    }
+
+    public void setAsientosOcupados(int asientosOcupados) {
+        this.asientosOcupados = asientosOcupados;
+    }
+
+    public double getPrecioDeLaEntrada() {
+        return PrecioDeLaEntrada;
+    }
+
+    public void setPrecioDeLaEntrada(double precioDeLaEntrada) {
         PrecioDeLaEntrada = precioDeLaEntrada;
+    }
+
+    public Proyeccion getProyeccion() {
+        return proyeccion;
+    }
+
+    public void setProyeccion(Proyeccion proyeccion) {
+        this.proyeccion = proyeccion;
     }
 
     // Método toString
@@ -42,9 +83,11 @@ public class Ticket {
         return "Ticket{" +
                 "id=" + id +
                 ", aforoDeLaSala=" + aforoDeLaSala +
-                ", AsientosOcupados=" + AsientosOcupados +
+                ", asientosOcupados=" + asientosOcupados +
                 ", PrecioDeLaEntrada=" + PrecioDeLaEntrada +
+                ", proyeccion=" + (proyeccion != null ? proyeccion.getId() : "null") +
                 '}';
     }
+
 }
 
