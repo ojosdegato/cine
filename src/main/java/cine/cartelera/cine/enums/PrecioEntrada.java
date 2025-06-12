e2b0522d54b3bb38f52d3e381d3266187a67ddc2
D-JMO <davidjimenezotero@gmail.com>
2025-06-12 13:27:03 +0200

    Crear enum PrecioEntrada con los valores de precios de entrada

---
+++ src/main/java/cine/cartelera/cine/enums/PrecioEntrada.java
@@ -0,0 +1,19 @@
+package cine.cartelera.cine.enums;
+
+import java.math.BigDecimal; // Importa la clase BigDecimal para manejar precios con precisión decimal
+
+public enum PrecioEntrada { //
+
+    PRECIO_NORMAL(new BigDecimal("7.50")), // Precio normal de una entrada
+    PRECIO_DIA_ESPECTADOR(new BigDecimal("3.00")); // Precio especial para el día del espectador
+
+    private final BigDecimal valor;
+
+    PrecioEntrada(BigDecimal valor) {
+        this.valor = valor; // Inicializa el valor del precio de la entrada
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+}
