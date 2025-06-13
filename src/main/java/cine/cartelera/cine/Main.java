3a49003a7d273cb5190c5a363ad14098992c29aa
ojosdegato <elcomandodelgeek@gmail.com>
2025-06-13 13:12:25 +0200

    Subir funcion Main

--- src/main/java/cine/cartelera/cine/Main.java
+++ src/main/java/cine/cartelera/cine/Main.java
@@ -1,7 +1,7 @@
 package cine.cartelera.cine;
 
-import cine.cartelera.cine.entities.Pelicula;
-import cine.cartelera.cine.repositories.PeliculaRepository;
+import cine.cartelera.cine.entities.*;
+import cine.cartelera.cine.repositories.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -17,7 +17,12 @@ public class Main {
 		ApplicationContext context = SpringApplication.run(Main.class, args);
 
 		// Obtener el repositorio desde el contexto
-		PeliculaRepository peliculaRepository = context.getBean(PeliculaRepository.class);
+		 PeliculaRepository peliculaRepository = context.getBean(PeliculaRepository.class);
+		 ActorRepository actorRepository = context.getBean(ActorRepository.class);
+		 PeliculaActorRepository peliculaActorRepository = context.getBean(PeliculaActorRepository.class);
+		 ProyeccionRepository proyeccionRepository = context.getBean(ProyeccionRepository.class);
+		 SalaRepository salaRepository = context.getBean(SalaRepository.class);
+		 TicketRepository ticketRepository = context.getBean(TicketRepository.class);
 
 	}
 }
