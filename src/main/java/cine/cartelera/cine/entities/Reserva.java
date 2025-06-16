4ddc78e53d21d7c0c2da127567870a3f440078ea
D-JMO <davidjimenezotero@gmail.com>
2025-06-16 18:56:11 +0200

    Crear entidades Reserva y Usuario (cine)

---
+++ src/main/java/cine/cartelera/cine/entities/Reserva.java
@@ -0,0 +1,57 @@
+package cine.cartelera.cine.entities;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@ToString (exclude = {"proyeccion", "usuario"}) //
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name="Reservas")
+public class Reserva {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "Proyeccion_id", nullable = false)
+    private Proyeccion proyeccion;
+
+    @ManyToOne
+    @JoinColumn(name = "Usuario_id", nullable = false)
+    private Usuario usuario;
+
+    @Column(nullable = false, unique = true)
+    private int numeroAsiento;
+
+    @Column(nullable = false)
+    private Double precioEntrada; // Precio de la entrada, puede ser diferente para "DIA_ESPECTADOR"
+
+    @Column(nullable = false)
+    private Integer cantidadEntradas;
+
+    @Column(nullable = false)
+    private String estadoReserva; // Puede ser "PENDIENTE", "CONFIRMADA", "CANCELADA"
+
+    @Column(nullable = false)
+    private String metodoPago; // Puede ser "TARJETA", "EFECTIVO", etc.
+
+    @Column(nullable = false)
+    private String fechaReserva; // Fecha en formato "YYYY-MM-DD HH:MM:SS" o similar para evitar problemas de zona horaria
+
+    @Column(nullable = false)
+    private String tipoEntrada; // Puede ser "NORMAL" o "DIA_ESPECTADOR"
+
+
+
+
+
+
+
+
+}
