ad6c9154e99722780cf403f16a4513c9a4f79497
D-JMO <davidjimenezotero@gmail.com>
2025-06-20 14:26:13 +0200

    Añadidos métodos personalizados en ActorRepository y nuevo atributo `nacionalidad` en la entidad Actor. Parte de Iñigo

--- src/main/java/cine/cartelera/cine/repositories/ActorRepository.java
+++ src/main/java/cine/cartelera/cine/repositories/ActorRepository.java
@@ -4,13 +4,20 @@ import cine.cartelera.cine.entities.Actor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository // Opcional, pero buena práctica para claridad
 public interface ActorRepository extends JpaRepository<Actor, Long> {
+    List<Actor> findByNombreContainingIgnoreCase(String nombre);
+
+    List<Actor> findByApellidoContainingIgnoreCase(String apellido);
+
+    List<Actor> findByNacionalidadIgnoreCase(String nacionalidad);
+
     // JpaRepository<TipoDeEntidad, TipoDeId>
     // Spring Data JPA provee automáticamente métodos como:
     // save(), findById(), findAll(), deleteById(), etc.
 
     // Puedes añadir métodos de consulta personalizados, por ejemplo:
-    // List<Actor> findByNombreContainingIgnoreCase(String nombre);
     // List<Actor> findByFechaNacimientoBefore(LocalDate fecha);
 }
