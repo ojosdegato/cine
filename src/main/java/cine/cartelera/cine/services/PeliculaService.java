32c591270884964e0655e51e5e2df88fae43cbfc
Javier Cachón Garrido <javier.cachon@izetic.com>
2025-06-24 20:27:19 +0200

    Update PeliculaService.java
    Corregido el error de eliminar pelicula, por un error en el PeliculaServiceImpl, todo fusinonado en PeliculaService.java.

--- src/main/java/cine/cartelera/cine/services/PeliculaService.java
+++ src/main/java/cine/cartelera/cine/services/PeliculaService.java
@@ -1,16 +1,34 @@
 package cine.cartelera.cine.services;
 
 import cine.cartelera.cine.entities.Pelicula;
+import cine.cartelera.cine.repositories.PeliculaRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface PeliculaService {
-    List<Pelicula> listarTodas();
+@Service
+public class PeliculaService {
 
-    Optional<Pelicula> buscarPorId(Long id);
+    private final PeliculaRepository peliculaRepository;
 
-    Pelicula guardar(Pelicula pelicula);
+    public PeliculaService(PeliculaRepository peliculaRepository) {
+        this.peliculaRepository = peliculaRepository;
+    }
 
-    void eliminar(Long id);
+    public List<Pelicula> listarTodas() {
+        return peliculaRepository.findAll();
+    }
+
+    public Optional<Pelicula> buscarPorId(Long id) {
+        return peliculaRepository.findById(id);
+    }
+
+    public Pelicula guardar(Pelicula pelicula) {
+        return peliculaRepository.save(pelicula);
+    }
+
+    public void eliminar(Long id) {
+        peliculaRepository.deleteById(id);
+    }
 }
